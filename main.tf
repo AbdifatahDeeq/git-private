1848a2dfb52846f824cece94b2369ff95baf08ff
AbdifatahDeeq <deeq8763@gmail.com>
2026-06-15 11:58:22 +0200

    product

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ provider "azurerm" {
 }
 
 
-# Create a virtual network within the resource group
+
 resource "azurerm_virtual_network" "example" {
   name                = "example-network"
   resource_group_name = azurerm_resource_group.example.name
